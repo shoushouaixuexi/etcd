26e81c2a1a43f6d2208c1e68a7015668f0b6ace8
root <2313284656@qq.com>
2022-05-16 21:27:35 +0800

    小更新

--- client/client.go
+++ client/client.go
@@ -12,25 +12,27 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var (
-	// addr  = flag.String("addr", "106.53.97.81:10001", "the address to connect to")
-	srvId = flag.String("srvId", "10001", "get srvinfo from etcd")
-)
+var srvId = []string{"10003", "10004"}
+
+// addr  = flag.String("addr", "106.53.97.81:10001", "the address to connect to")
+//srvId = flag.String("srvId", "10001", "get srvinfo from etcd")
 
 func main() {
 	//log.Printf("nihao")
 	flag.Parse()
-
+	log.Printf("开始服务发现：")
 	// 开始发现服务
-	disc, err := discovery.NewEtcdRegister()
+	disc, err := discovery.NewEtcdDiscovery()
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("did not connect etcd: %v", err)
 	}
-	srvaddr, err := disc.GetAddrFromServiceId(*srvId)
+	srvaddr, err := disc.GetAddrFromServiceId(srvId)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("Service can not find: %v", err)
+		return
 	}
-	log.Printf("srvaddr: %s", srvaddr)
+	log.Printf("需要访问的服务地址信息: %s", srvaddr)
+	log.Printf("服务发现结束！")
 	// 输出服务信息
 
 	// Set up a connection to the server.
@@ -50,10 +52,12 @@ func main() {
 	defer cancel()
 	var request pb.ServiceRequest
 	request.Operation = append(request.Operation, &pb.Operation{
-		FirstNumber:   2,
-		SecondNumber:  3,
+		FirstNumber:   3,
+		SecondNumber:  2,
 		ServiceNumber: 0,
 	})
+	log.Printf("调用服务的参数为%v,%v", 3, 2)
+	log.Printf("发起rpc服务调用：")
 	r, err := c.ServiceMethod(ctx, &request)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
@@ -63,5 +67,5 @@ func main() {
 	for _, value := range r.Result {
 		log.Printf("result: %v", value)
 	}
-
+	log.Printf("rpc服务调用结束！")
 }
--- discovery/etcd_discovery.go
+++ discovery/etcd_discovery.go
@@ -2,8 +2,10 @@ package discovery
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -14,7 +16,7 @@ type EtcdDiscovery struct {
 	ctx context.Context  //上下文
 }
 
-func NewEtcdRegister() (*EtcdDiscovery, error) {
+func NewEtcdDiscovery() (*EtcdDiscovery, error) {
 
 	client, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{"http://106.53.97.81:12379", "http://106.53.97.81:22379", "http://106.53.97.81:32379"},
@@ -27,23 +29,37 @@ func NewEtcdRegister() (*EtcdDiscovery, error) {
 	}
 
 	ctx, _ := context.WithCancel(context.Background())
-
 	srv := &EtcdDiscovery{
 		cli: client,
 		ctx: ctx,
 	}
 	return srv, nil
 }
-
-func (srv *EtcdDiscovery) GetAddrFromServiceId(key string) (string, error) {
-	resp, err := srv.cli.Get(srv.ctx, key)
-	if err != nil {
-		return "", err
+func RetRandAddr(key []string) (string, error) {
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(len(key))
+	return key[num], nil
+}
+func (srv *EtcdDiscovery) GetAddrFromServiceId(key []string) (string, error) {
+	var addr = []string{}
+	for _, val := range key {
+		resp, err := srv.cli.Get(srv.ctx, val)
+		if err != nil {
+			return "", err
+		}
+		for _, val := range resp.Kvs {
+			//fmt.Printf("GET response：%s=%s\n", val.Key, val.Value)
+			if string(val.Value) != "" {
+				addr = append(addr, string(val.Value))
+			}
+		}
 	}
-	addr := ""
-	for _, val := range resp.Kvs {
-		fmt.Printf("GET response：%s=%s\n", val.Key, val.Value)
-		addr = string(val.Value)
+	if len(addr) == 0 {
+		return "", errors.New("发现服务失败，key对应服务不存在")
 	}
-	return addr, nil
+	for _, srvaddr := range addr {
+		fmt.Printf("可使用的addr%s\n", srvaddr)
+	}
+	raddr, _ := RetRandAddr(addr)
+	return raddr, nil
 }
